18602fc4b0650d962c5829f5389b011ce436cfec
chandan nath <3198nathchandan@gmail.com>
2024-12-17 21:54:59 +0530

    stable

--- index.js
+++ index.js
@@ -2,8 +2,17 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry,Text,TextInput} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+
+// Disable font scaling for Text and TextInput components globally
+Text.defaultProps = Text.defaultProps || {};
+Text.defaultProps.allowFontScaling = false;
+
+TextInput.defaultProps = TextInput.defaultProps || {};
+TextInput.defaultProps.allowFontScaling = false;
+
 
 AppRegistry.registerComponent(appName, () => App);
